79d7b79953b78a07b29caa80da84bc18ed8e195c
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-05-29 10:19:08 -0400

    chore: add compiled block build output

---
+++ build/videomuxr-video/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '07dbcf644b2d5ea09f87');
